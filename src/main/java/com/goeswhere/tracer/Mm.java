727c062557575fdf46f3a35f0a29e833e46af060
Chris West (Faux) <git@goeswhere.com>
2010-06-15 01:37:21 +0100

    Mm functions are unsupportedOperations

--- src/main/java/com/goeswhere/tracer/Mm.java
+++ src/main/java/com/goeswhere/tracer/Mm.java
@@ -2,7 +2,7 @@ package com.goeswhere.tracer;
 
 public class Mm {
 	public static SSEFloat _mm_and_ps(SSEFloat validHitMask, SSEFloat shadeGTZeroMask) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_add_ps(SSEFloat a, SSEFloat b) {
@@ -14,11 +14,11 @@ public class Mm {
 	}
 
 	static SSEFloat _mm_cmpgt_ps(SSEFloat distance, SSEFloat zero2) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_cmplt_ps(SSEFloat distance, SSEFloat nearestObstruction) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_sub_ps(SSEFloat a, SSEFloat b) {
@@ -46,31 +46,31 @@ public class Mm {
 	}
 
 	static SSEFloat _mm_andnot_ps(SSEFloat control, SSEFloat v1) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_cmpeq_ps(SSEFloat sseNearest, SSEFloat sseSI) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_cmpge_ps(SSEFloat d, SSEFloat _mm_setzero_ps) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_mul_ps(Object _mm_mul_ps, SSEFloat lightPower) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_cmpneq_ps(SSEFloat positionX, SSEFloat miss2) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static __m128i _mm_set1_epi32(int x) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static __m128i _mm_loadu_si128(__m128i __m128i) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_setr_ps(int i, int j, int k, int l) {
@@ -78,7 +78,7 @@ public class Mm {
 	}
 
 	static SSEFloat _mm_min_ps(SSEFloat red, SSEFloat twoFiftyFive) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_rsqrt_ps(SSEFloat a) {
@@ -90,25 +90,26 @@ public class Mm {
 	}
 
 	static void _mm_store_ps(int[] nearest, SSEFloat sseNearest) {
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_set_ps1(float maxValue) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_sqrt_ps(SSEFloat _mm_add_ps) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_div_ps(SSEFloat one, SSEFloat a) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static SSEFloat _mm_or_ps(SSEFloat vTemp1, SSEFloat vTemp2) {
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	static int _mm_movemask_ps(SSEFloat mask) {
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 }
