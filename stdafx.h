fbd3243d000a16c149cbd8ea2b8c21205072d161
Chris West (Faux) <git@goeswhere.com>
2012-07-25 20:53:28 +0100

    Upgraded project files

--- stdafx.h
+++ stdafx.h
@@ -3,6 +3,7 @@
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 #define NOMINMAX
 
+#include "targetver.h"
 #include <stdio.h>
 #include <string>
 #include <iostream>
